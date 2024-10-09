7e24d4c727a26a10eef3a1f7d5c6464ff161b78c
Matías Hernán Martinez <mati8910@hotmail.com>
2024-10-08 23:24:07 -0300

    change css footer and wp contact

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div >
       <footer className="footer">
         <a
-          href="https://wa.me/"
+          href="https://wa.me/542615734324"
           target="_blank"
           rel="noopener noreferrer"
           className="contact-button"
